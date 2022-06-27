972b988263c53dbaee9d02811fb46142e6d1bf3d
Ruban Pathak <102974324+ruban117@users.noreply.github.com>
2022-06-27 19:29:33 +0530

    Update creating_an_array _using_realloc().c

--- creating_an_array _using_realloc().c
+++ creating_an_array _using_realloc().c
@@ -4,6 +4,7 @@
 int main(){
 	int *ptr ;/*creating a pointer with respect of return type for accessing malloc*//*or we allocate some memory in ptr*/
 	int n,m;
+	char ch;
 	printf("Enter The Range Of The Array: ");
 	scanf("%d",&n);
 	ptr = (int*) malloc(n * sizeof(int));
@@ -11,20 +12,27 @@ int main(){
 		printf("Enter Element %d: ",i+1);
 		scanf("%d",&ptr[i]);
 	}
-	printf("The Elements Are: \n");
-	for(int i=0 ; i<n ; i++){
-		printf("%d ",ptr[i]);
-	}
-	printf("\nEnter another size to reallocate: ");
-	scanf("%d",& m);
-	ptr=realloc(ptr , m * sizeof(int));/*reallocate ptr using realloc()*/
-	for(int i=0 ; i<m ; i++){
+	printf("Do You Want To Add More Elements?: ");
+	scanf("%s",&ch);
+	if(ch=='y'|| ch=='Y'){
+	printf("Enter Ading Elements: ");
+	scanf("%d",&m);
+	ptr=realloc(ptr , m+n * sizeof(int));/*reallocate ptr using realloc()*/
+	for(int i=n ; i<m+n ; i++){
 		printf("Enter Element %d: ",i+1);
 		scanf("%d",&ptr[i]);
 	}
 	printf("The Elements Are: \n");
-	for(int i=0 ; i<m ; i++){
+	for(int i=0 ; i<n+m ; i++){
 		printf("%d ",ptr[i]);
 	}
+}
+else
+{
+	printf("The Elements Are: \n");
+	for(int i=0 ; i<n ; i++){
+		printf("%d ",ptr[i]);
+	}
+}
 	return 0;
 }
