816c3a497f3f879e99180c902f92c66ac2e9c0e2
Ruban Pathak <102974324+ruban117@users.noreply.github.com>
2022-11-24 11:43:49 +0530

    Update sparce_mattrix.c

--- sparce_mattrix.c
+++ sparce_mattrix.c
@@ -2,7 +2,7 @@
 
 #include<stdio.h>
 int main(){
-	int row,column,i,j,size,total;
+	int row,column,i,j,size,total,l,m,n;
 	printf("How Many Rows?: ");
 	scanf("%d",&row);
 	printf("How Many Columns?: ");
@@ -32,25 +32,42 @@ int main(){
 			}
 		}
 	}
-	int newmat[3][size];
+	int newmat[size][3];
 	int k=0;
-	for(i=0;i<row;i++){
+	for(i=0;i<row;i++)
 		for(j=0;j<column;j++){
 			if(sparcemat[i][j]!=0){
-				newmat[0][k]=i;
-				newmat[1][k]=j;
-				newmat[2][k]=sparcemat[i][j];
+				newmat[k][0]=i;
+				newmat[k][1]=j;
+				newmat[k][2]=sparcemat[i][j];
 				k++;
 			}
 		}
-	}
+		/*int flg;
+    for(l=0;l<newmat[0][0];l++){
+        for(m=0;m<newmat[0][1];m++){
+            flg=1;
+                for(n=1;n<=newmat[0][2];n++){
+                    if(newmat[n][0]==i&&newmat[n][1]==j){
+                        flg=0;
+                        printf("%d ",newmat[n][2]);
+                        break;
+                    }
+                }
+            if(flg==1)
+                printf("0 ");
+        }
+        printf("\n");
+    }*/
 	printf("The Matrix Is:\n");
-	for(i=0;i<3;i++){
-		for(j=0;j<size;j++){
+	printf("%d %d %d\n",row,column,k);
+	for(i=0;i<size;i++){
+		for(j=0;j<3;j++){
 			printf("%d ",newmat[i][j]);
 		}
 		printf("\n");
 	}
+	
 }
 else{
 	printf("The given matrix is not Sparse matrix You go and study!!!");
